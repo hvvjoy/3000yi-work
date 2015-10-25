e2f8e05634f1118cb5d6e2ab3440e292294d5b8b
unknown <hugo.huang>
2015-10-25 16:00:06 +0800

    route

---
+++ frontend/config/route.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+		'enablePrettyUrl' => true,
+		//'suffix' => '.shtml',     //如果没有写suffix，就不会被程序找到，官方手册不推荐这个配置
+		'rules' => [
+				'task-<id:\w+>' => 'home/task/show',
+				'<controller:\w+>/<action:\w+>' => 'home/<controller>/<action>',
+		],
+];
---
+++ frontend/widgets/views/sidebar.php
@@ -0,0 +1,41 @@
+<div class="col-sm-12">
+    <div class="bs-docs-sidebar col-sm-2 sidebar mt-20  affix">
+        <div class="mini-submenu">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </div>
+        <div class="list-group">
+            <span href="#" class="list-group-item">
+                Submenu
+                <span class="pull-right visible-lg-block">
+                    <i class="fa fa-desktop"></i>
+                </span>
+            </span>
+            <a href="/task/index" class="list-group-item" id='nav-side-task'>
+                <i class="fa fa-tags"></i> &nbsp;&nbsp;任务
+            </a>
+            <a href="#" class="list-group-item" id='nav-side-problem'>
+                <i class="fa fa-bug"></i> &nbsp;&nbsp;报错
+            </a>
+            <a href="#" class="list-group-item" id='nav-side-advice'>
+                <i class="fa fa-book"></i> &nbsp;&nbsp;建议
+            </a>
+            <a href="#" class="list-group-item" id='nav-side-journal'>
+                <i class="fa fa-sticky-note"></i> &nbsp;&nbsp;日志
+            </a>
+            <a href="#" class="list-group-item" id='nav-side-plan'>
+                <i class="fa fa-arrows"></i> &nbsp;&nbsp;安排
+            </a>
+            <a href="#" class="list-group-item" id='nav-side-team'>
+                <i class="fa fa-users"></i> &nbsp;&nbsp;团队
+            </a>
+            <a href="#" class="list-group-item" id='nav-side-account'>
+                <i class="fa fa-home"></i> &nbsp;&nbsp;帐号
+            </a>
+            <a href="#" class="list-group-item" id='nav-side-setting'>
+                <i class="fa fa-cog"></i> &nbsp;&nbsp;设置
+            </a>
+        </div>        
+    </div>
+</div>
